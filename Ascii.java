4fb5e41fefe6f522167393ee66d73562cb20a0f0
eddyddoneyoh <eddyddoneyoh@users.noreply.github.com>
2015-03-02 08:45:38 +0100

    Create Ascii.java

---
+++ Ascii.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+
+public class Ascii
+{
+
+
+	public static void main(String args[]){
+
+
+		
+	Scanner in1 = new Scanner(System.in);
+
+	String input  = in1.nextLine();	
+
+	int length = input.length();
+
+	int sum = 0;
+
+	int i = 0;
+
+
+		for(i = 0 ; i < length ; i++){
+	
+  		char test = input.charAt(i);
+
+		sum = sum + (int)test;
+		
+	       
+		}
+
+		System.out.println("the sum of the numbers is :" + sum);
+		
+
+	}
+
+}
